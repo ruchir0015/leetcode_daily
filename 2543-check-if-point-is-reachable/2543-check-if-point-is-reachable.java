27fee71ebc6ffb75f8454cea896eb0b63d71fa12
Ruchir Jain <rj863093701@gmail.com>
2026-08-12 16:39:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.15%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
@@ -1,13 +1,8 @@
 class Solution {
-    public boolean isReachable(int targetX, int targetY) {
-        int g = gcd(targetX, targetY);
-        if (g == 1) {
-            return true;
-        }
-        while (g % 2 == 0) {
-            g /= 2;
-        }
-        return g == 1;
+    public boolean isReachable(int x, int y) {
+        while(x % 2 == 0) x = x / 2;
+      while(y % 2 == 0) y = y / 2;
+      return gcd(x,y) == 1;  
     }
     public int gcd(int a, int b) {
         while (b != 0) {
