0bca8aece3ce4ca621be3c3685351d32fb0a1de9
Ruchir Jain <rj863093701@gmail.com>
2026-08-23 21:32:33 +0530

    Time: 1 ms (99.7%), Space: 43.4 MB (98.7%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean isPalindromic(String s) {
-
         int n = s.length() * 8;
-
         for (int i = 0, j = n - 1; i < j; i++, j--) {
-
             int char1 = s.charAt(i / 8);
             int char2 = s.charAt(j / 8);
-
             int bit1 = (char1 >> (7 - i % 8)) & 1;
             int bit2 = (char2 >> (7 - j % 8)) & 1;
-
-            if (bit1 != bit2) {
-                return false;
-            }
+            if (bit1 != bit2) return false;
         }
-
         return true;
     }
 }
