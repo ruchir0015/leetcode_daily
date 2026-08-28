68d64f5f0aaa256ea095781f8af327ee0ee93b00
Ruchir Jain <rj863093701@gmail.com>
2026-08-28 14:36:41 +0530

    Time: 12 ms (26.57%), Space: 44 MB (67.77%) - LeetHub

--- 0481-magical-string/0481-magical-string.java
+++ 0481-magical-string/0481-magical-string.java
@@ -1,20 +1,20 @@
 class Solution {
-
     public int magicalString(int n) {
         if (n <= 0) return 0;
         if (n <= 3) return 1;
-        int count = 1;
         StringBuilder s = new StringBuilder("122");
-        int i = 2;
-        char num = '1';
-        while (s.length() < n) {
+        int i = 2;      
+        int j = 3;      
+        int num = 1;    
+        int count = 1;  
+        while (j < n) {
             int times = s.charAt(i) - '0';
-            for (int j = 0; j < times && s.length() < n; j++) {
+            for (int k = 0; k < times && j < n; k++) {
                 s.append(num);
-                if (num == '1')  count++;
+                if (num == 1) count++;
+                j++;
             }
-            if (num == '1') num = '2';
-            else  num = '1';
+            num = 3 - num;
             i++;
         }
         return count;
